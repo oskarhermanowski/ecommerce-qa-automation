766b6f626d9b139ea157cdd5837192f056890f9e
oskarhermanowski <oskar.hermanowski@gmail.com>
2026-09-16 23:21:25 +0200

    Clean up test framework code

--- python-api-tests/tests/test_products.py
+++ python-api-tests/tests/test_products.py
@@ -71,7 +71,7 @@ def test_create_product(api_base_url):
         json=payload
     )
 
-    assert response.status_code ==201
+    assert response.status_code == 201
 
     data = response.json()
 
--- ui-tests/conftest.py
+++ ui-tests/conftest.py
@@ -2,8 +2,6 @@ import pytest
 import sys
 from pathlib import Path
 
-import pytest
-
 UI_TESTS_DIR = Path(__file__).resolve().parent
 sys.path.append(str(UI_TESTS_DIR))
 
